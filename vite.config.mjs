5962fd3dd10aa35ec17571df797114728dd0ac09
alexPC <alex@alexPCMac.local>
2026-02-01 18:08:34 +0500

    chore: serve templates as publicDir

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    // serve files from `templates/` as static public files
+    publicDir: 'templates',
     server: {
         port: 5173,
         proxy: {
